008de6376519d9c9544d17a7c660884f1075104e
Milliella <115160750+Milliella@users.noreply.github.com>
2022-10-20 12:31:09 +0100

    Add files via upload

---
+++ shoppinglist.js
@@ -0,0 +1,16 @@
+javascript.js> shopppingList
+let shoppinglist= ["apples", "cake", "pasta", "grapefruit"]
+
+let shoppingListItem= shoppingList[2]
+
+shoppingList[3]= "bananas"
+
+shoppingList.push("water", "coke")
+
+shoppingList.pop()
+
+shoppingList.splice(2,0,"rum")
+
+let shoppingLength = shoppingList.length;
+
+for(let i = 0; i < 100;)
