48309e1f5a1b18fd984efdda9f5e52e9784707d9
Milliella <115160750+Milliella@users.noreply.github.com>
2022-10-20 12:28:57 +0100

    Add files via upload

---
+++ myJavascriptFile.js
@@ -0,0 +1,6 @@
+// This is an empty Javascript File
+let characterNames = ["Wirt", "Greg", "Beatrice", "Jason Funderberker", "Enoch"]
+for (let i = 0; i < characterNames.length; i++) {
+    	
+    console.log(characterNames[i]);
+}
